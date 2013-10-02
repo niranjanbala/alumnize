3258ce87a979efa99cb80154aa72d409de3ca549
niranjan <niranjan.bala@gmail.com>
2013-10-02 15:19:36 +0530

    Home Page.

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ alumnize.Router = Backbone.Router.extend({
     }
 });
 $(document).on("ready", function () {
-    alumnize.loadTemplates(["LandingPage"],
+    alumnize.loadTemplates(["FeedBack","Carousel","Navigation","Features","Footer"],
         function () {
             alumnize.router = new alumnize.Router();
             Backbone.history.start();
--- public/js/views/LandingPage.js
+++ public/js/views/LandingPage.js
@@ -1,8 +1,11 @@
 alumnize.LandingPage = Backbone.View.extend({
 
     render:function () {
-        this.$el.html(this.template());
+        this.$el.append(new alumnize.FeedBack().render().el);
+        this.$el.append(new alumnize.Navigation().render().el);
+        this.$el.append(new alumnize.Carousel().render().el);
+        this.$el.append(new alumnize.Features().render().el);
+        this.$el.append(new alumnize.Footer().render().el);
         return this;
     }
-
 });
