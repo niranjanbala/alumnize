cc4ebbbf9e5fab0072a2d66a5f73352387844557
niranjan <niranjan.bala@gmail.com>
2013-10-03 17:56:00 +0530

    cleanup fb

---
+++ public/js/social/facebook.js
@@ -0,0 +1,21 @@
+alumnize.facebookConnect = function(successHandler,failureHandler) {
+	$.ajaxSetup({ cache: true });
+	$.getScript('//connect.facebook.net/en_UK/all.js', function(){
+		FB.init({
+			appId: '514256625329292',
+			status     : true, // check login status
+			cookie     : true, // enable cookies to allow the server to access the session
+			xfbml      : true  // parse XFBML
+		});
+		FB.login(function(response) {
+			if (response.authResponse) {
+				console.log('Welcome!  Fetching your information.... ');
+				FB.api('/me', function(response) {
+					successHandler(response);
+				});
+			} else {
+				failureHandler(response);
+			}
+		}, {scope: 'email,publish_stream,user_about_me,user_education_history,user_location,user_work_history'});
+	});
+}
