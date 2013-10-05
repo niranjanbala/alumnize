315d4c636cce7deee83d6c3b179cb7c3aad2c36c
niranjan <niranjan.bala@gmail.com>
2013-10-05 22:16:08 +0530

    uhp

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     path = require('path'),
-    http = require('http');
+    http = require('http'),
+    facebook = require('facebook-node-sdk');
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
@@ -8,7 +9,14 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
+    app.use(facebook.middleware({ appId: '514256625329292', secret: 'YOUR_APP_SECRET' }));
     app.use(express.static(path.join(__dirname, 'public')));
+});
+app.get('/', facebook.loginRequired(), function (req, res) {
+  req.facebook.api('/me', function(err, user) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, ' + user.name + '!');
+  });
 });
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
