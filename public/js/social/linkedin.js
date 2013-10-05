45d4bbc5230114fa563eb2b12f234053192654b5
niranjan <niranjan.bala@gmail.com>
2013-10-05 12:21:58 +0530

    li

--- public/js/social/linkedin.js
+++ public/js/social/linkedin.js
@@ -1,6 +1,20 @@
 alumnize.linkedinConnect = function(successHandler,failureHandler) {
 	IN.UI.Authorize().params({"scope":["r_fullprofile", "r_emailaddress","r_contactinfo"]}).place();
 	IN.Event.on(IN, "auth", function() {
-		IN.API.Profile("me").fields([ "id","firstName", "location","lastName","skills","positions","educations","languages","phone-numbers","certifications","emailAddress","mainAddress"]).result(successHandler).error(failureHandler);
+		var fetchFields=[
+			"id",
+			"firstName",
+			"location",
+			"lastName",
+			"skills",
+			"positions",
+			"educations",
+			"languages",
+			"phone-numbers",
+			"certifications",
+			"emailAddress",
+			"mainAddress"
+		];
+		IN.API.Profile("me").fields(fetchFields).result(successHandler).error(failureHandler);
 	});
 };
