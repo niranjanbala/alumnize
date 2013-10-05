6b38ba1a4829c721f902029de10dbf20f5c2736e
niranjan <niranjan.bala@gmail.com>
2013-10-05 12:05:02 +0530

    LI

--- public/js/social/linkedin.js
+++ public/js/social/linkedin.js
@@ -1,13 +1,6 @@
 alumnize.linkedinConnect = function(successHandler,failureHandler) {
 	IN.UI.Authorize().params({"scope":["r_fullprofile", "r_emailaddress","r_contactinfo"]}).place();
-	IN.Event.on(IN, "auth", this.onLinkedInAuth);
-},
-onLinkedInAuth = function () {
-	IN.API.Profile("me").fields([ "id","firstName", "location","lastName","skills","positions","educations","languages","phone-numbers","certifications","emailAddress","mainAddress"]).result(this.success).error(this.failure);
-},
-success = function (response) {
-	successHandler(response);
-},
-failure = function (response) {
-	failureHandler(response);
-}
+	IN.Event.on(IN, "auth", function() {
+		IN.API.Profile("me").fields([ "id","firstName", "location","lastName","skills","positions","educations","languages","phone-numbers","certifications","emailAddress","mainAddress"]).result(successHandler).error(failureHandler);
+	});
+};
