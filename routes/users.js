303e934bde3eb87578610a375b00c8b783f6a019
niranjan <niranjan.bala@gmail.com>
2013-10-06 13:12:17 +0530

    mongo

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var MongoClient = require('mongodb').MongoClient,
     db;
-MongoClient.connect("mongodb://heroku_app18353461:welcome001*@ds047448.mongolab.com:47448/heroku_app18353461", function(err, mDb) {
+MongoClient.connect("mongodb://user:welcome001*@ds047448.mongolab.com:47448/heroku_app18353461", function(err, mDb) {
     db=mDb;
 });
 exports.findOrCreateFaceBookUser = function(accessToken, refreshToken, profile, done) {
-	db.collection('users', function(err, collection) {
+	db.collection('users', function(err, collection) {		
 		collection.findOne({fbId : profile.id}, function(err, item) {
 			if(item){
             	done(null,item);
