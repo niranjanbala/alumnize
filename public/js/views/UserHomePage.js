ea3320bc0846e8b89656a21103591b76c0a7d519
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-06 20:13:42 +0530

    uhp

--- public/js/views/UserHomePage.js
+++ public/js/views/UserHomePage.js
@@ -2,7 +2,8 @@ alumnize.UserHomePage= Backbone.View.extend({
 
  render:function () {
  	 var data = _.clone(this.model.attributes);
-     data.id = this.model.id;
+	 data.id = this.model.id;
+	 console.log(data);
 	 this.$el.html(this.template(data));
 	 this.$el.append(new alumnize.Footer().render().el);
 	 return this;
--- server.js
+++ server.js
@@ -38,7 +38,21 @@ passport.use(new GoogleStrategy({
 passport.use(new LinkedInStrategy({
     consumerKey: LINKEDIN_API_KEY,
     consumerSecret: LINKEDIN_SECRET_KEY,
-    callbackURL: "http://alumnize-beta.herokuapp.com/auth/linkedin/callback" 
+    callbackURL: "http://alumnize-beta.herokuapp.com/auth/linkedin/callback",
+    profileFields: [
+			"id",
+			"firstName",
+			"location",
+			"lastName",
+			"skills",
+			"positions",
+			"educations",
+			"languages",
+			"phone-numbers",
+			"certifications",
+			"emailAddress",
+			"mainAddress"
+		]
   },
   function(token, tokenSecret, profile, done) {
     users.findOrCreateLinkedInUser(token, tokenSecret, profile, done);
