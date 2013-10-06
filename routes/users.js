5f103b97d589f556fbe3580f03776e2c948c3452
niranjan <niranjan.bala@gmail.com>
2013-10-06 14:56:28 +0530

    fb

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,29 @@ MongoClient.connect("mongodb://user:welcome001*@ds047448.mongolab.com:47448/hero
 });
 exports.findOrCreateFaceBookUser = function(accessToken, refreshToken, profile, done) {
 	db.collection('users', function(err, collection) {		
-		collection.findOne({fbId : profile.id}, function(err, item) {
+		collection.findOne({facebook.id : profile.id}, function(err, item) {
 			if(item){
             	done(null,item);
         	}else {
         		console.log(profile);
-        		collection.insert([{
-                	fbId : profile.id ,
-                	email : [profile.email]
-                	gender: profile.gender,
-	                name : profile.displayName,	
-    	            firstName: profile.first_name,
-        	        middleName: profile.middle_name,
-            	    lastName: profile.last_name,
-                	lastUpdated: profile.updated_time,
-                	associations:[],
+        		collection.insert([{           
+        			"facebook": {
+        				id: profile.id,
+        				link: profile.link,
+        				updated_time: profile.updated_time,
+        				username: profile.username,
+						verified: true,		
+        			}     	
+                	"email" : [profile.email]
+                	"gender": profile.gender,
+	                "name" : profile.displayName,	
+    	            "firstName": profile.first_name,
+        	        "middleName": profile.middle_name,
+            	    "lastName": profile.last_name,
+            	    "locale" : profile.locale,
+            	    "timezone": profile.timezone,
+            	    "location": profile.location,
+                	"associations":[],
         		}], {safe:true}, function(err, result) {
         			if(err) throw err;
                 	done(null, result);
