bfa931e7be5654e5802358cca89cddba511422d1
niranjan <niranjan.bala@gmail.com>
2013-10-06 23:55:56 +0530

    cleanedup login.js

--- public/js/views/Login.js
+++ public/js/views/Login.js
@@ -1,39 +1,9 @@
 alumnize.Login = Backbone.View.extend({
-	events: {		
-	},
-
 	initialize:function () {
 		this.render();
 	},
-	facebookSuccess: function(response) {
-		console.log('Good to see you, ' + response.name + '.');
-		console.log(response);
-		alumnize.userHomePage = new alumnize.UserHomePage();
-		$('body').html(alumnize.userHomePage.render().el);
-	},
-	facebookFailure: function(response) {
-			console.log('User cancelled login or did not fully authorize.');
-			console.log('Full response : ' + response);
-	},
-	facebookLogin:function () {
-		alumnize.facebookConnect(this.facebookSuccess,this.facebookFailure);
-	},
-	linkedinSuccess: function(response) {
-		console.log('Good to see you, ' + response );
-		console.log(response);
-		alumnize.userHomePage = new alumnize.UserHomePage();
-		$('body').html(alumnize.userHomePage.render().el);
-	},
-	linkedinFailure: function(response) {
-		console.log('User cancelled login or did not fully authorize.');
-		console.log('Full response : ' + response);
-	},
-	linkedinLogin:function () {
-		alumnize.linkedinConnect(this.linkedinSuccess,this.linkedinFailure);
-	},
-
 	render:function () {
-        	this.$el.html(this.template());
-        	return this;
+			this.$el.html(this.template());
+			return this;
 	}
 });
