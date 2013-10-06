e0da69a715939ca24b5277c9c830529f8d0fe8ca
niranjan <niranjan.bala@gmail.com>
2013-10-06 23:35:34 +0530

    fb

--- routes/users.js
+++ routes/users.js
@@ -4,25 +4,25 @@ MongoClient.connect("mongodb://user:welcome001*@ds047448.mongolab.com:47448/hero
     db=mDb;
 });
 exports.findOrCreateFaceBookUser = function(accessToken, refreshToken, profile, done) {
-	db.collection('users', function(err, collection) {		
+	db.collection('users', function(err, collection) {
 		collection.findOne({"facebook.id" : profile.id}, function(err, item) {
 			if(item){
             	done(null,item);
         	}else {
-        		collection.insert([{           
+        		collection.insert([{
         			"facebook": {
         				"id": profile.id,
 					"profile": profile._json
-        			},   	
+        			},
 	                	"email" : profile.emails,
         	        	"gender": profile.gender,
-		                "name" : profile.displayName,	
+		                "name" : profile.displayName,
     			        "firstName": profile.name.givenName,
         		        "middleName": profile.name.middleName,
 		            	"lastName": profile.name.familyName,
-	            	    	"locale" : profile._json.locale,
+		            	"locale" : profile._json.locale,
 		            	"timezone": profile._json.timezone,
-		            	"location": profile._json.location.name,
+		            	"location": profile._json.location,
 	                	"associations": {
         	        		"work": profile._json.work,
         	        		"education": profile._json.education
@@ -31,24 +31,24 @@ exports.findOrCreateFaceBookUser = function(accessToken, refreshToken, profile, 
         			if(err) throw err;
         		        	done(null, result);
         		});
-        	}            
+        	}
         });
 	});
 };
 exports.findOrCreateGoogleUser = function(identifier, profile, done) {
-	db.collection('users', function(err, collection) {		
+	db.collection('users', function(err, collection) {
 		collection.findOne({"google.id" : identifier}, function(err, item) {
 		if(item){
             		done(null,item);
         	}else {
-        		collection.insert([{           
+        		collection.insert([{
         			"google": {
         				"id": identifier,
 					"profile": profile
         			},
 	                	"email" : "",
         	        	"gender": "",
-		                "name" : "",	
+		                "name" : "",
     			        "firstName": "",
         		        "middleName": "",
 		            	"lastName": "",
@@ -63,24 +63,24 @@ exports.findOrCreateGoogleUser = function(identifier, profile, done) {
         			if(err) throw err;
         		        	done(null, result);
         		});
-        	}            
+        	}
         });
 	});
 };
 exports.findOrCreateLinkedInUser = function(token, tokenSecret, profile, done) {
-	db.collection('users', function(err, collection) {		
+	db.collection('users', function(err, collection) {
 		collection.findOne({"linkedin.id" : profile.id}, function(err, item) {
 		if(item){
             		done(null,item);
         	}else {
-        		collection.insert([{           
+        		collection.insert([{
         			"linkedin": {
         				"id": profile.id,
 					"profile": profile
         			},
 	                	"email" : "",
         	        	"gender": "",
-		                "name" : "",	
+		                "name" : "",
     			        "firstName": "",
         		        "middleName": "",
 		            	"lastName": "",
@@ -95,7 +95,7 @@ exports.findOrCreateLinkedInUser = function(token, tokenSecret, profile, done) {
         			if(err) throw err;
         		        	done(null, result);
         		});
-        	}            
+        	}
         });
 	});
 };
