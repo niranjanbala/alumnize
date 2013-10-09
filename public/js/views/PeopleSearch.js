96f3af68c1e2c82f493cd8f0f25883ac6a448c20
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-10 02:42:35 +0530

    PS commit-12   consoling the data, need to display in view and need to implement pagination

--- public/js/views/PeopleSearch.js
+++ public/js/views/PeopleSearch.js
@@ -6,14 +6,10 @@ alumnize.Peoples = Backbone.Model.extend({
 
 alumnize.PeopleSearch = Backbone.View.extend({
     render:function () {
-    	console.log("Reached PeopleSearch...");
     	var data = _.clone(this.model.attributes);
     	data.id = this.model.id;
     	console.log("peoplesearch data ",data);
     	this.$el.html(this.template(data));        
         return this;
-    }, 
-    peopleSearch: function() {
- 		$('.container').html(new alumnize.PeopleSearch({model: this.model}).render().el); 	
- 	}
+    }
 });
