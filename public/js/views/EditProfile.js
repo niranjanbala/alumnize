be0ba87de1982f6018642d85a654492179b427d3
niranjan <niranjan.bala@gmail.com>
2013-10-09 23:32:35 +0530

    ep

--- public/js/views/EditProfile.js
+++ public/js/views/EditProfile.js
@@ -2,6 +2,7 @@
 
     render:function () {
     	var data = _.clone(this.model.attributes);
+    	data.id = this.model.id;
     	console.log(data);
         this.$el.html(this.template(data));        
         return this;
