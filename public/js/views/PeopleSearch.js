2be5642256c7a8fa54108b71490263ee51692e3f
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-11 00:32:14 +0530

    reverting changes

--- public/js/views/PeopleSearch.js
+++ public/js/views/PeopleSearch.js
@@ -9,37 +9,7 @@ alumnize.PeopleSearch = Backbone.View.extend({
     	var data = _.clone(this.model.attributes);
     	data.id = this.model.id;
     	console.log("peoplesearch data ",data);
-
-    	var len = data.length;
-        var startPos = (this.options.page - 1) * 8;
-        var endPos = Math.min(startPos + 8, len);
-
-        $(this.el).html('<ul class="thumbnails"></ul>');
-
-        for (var i = startPos; i < endPos; i++) {
-            $('.thumbnails', this.el).append(new PeopleListView({model: data[i]}).render().el);
-        }
-
-        //$(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
-
-        return this;
-
-    	this.$el.html(this.template(data));
-    }
-});
-
-alumnize.PeopleListView = Backbone.View.extend({
-
-    tagName: "li",
-
-    initialize: function () {
-        this.model.bind("change", this.render, this);
-        this.model.bind("destroy", this.close, this);
-    },
-
-    render: function () {
-        $(this.el).html(this.template(this.model.toJSON()));
+    	this.$el.html(this.template(data));        
         return this;
     }
-
 });
