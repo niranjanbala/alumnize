c889fb740b2434a6589be34b78b416104d407758
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-14 00:17:01 +0530

    ps

--- public/js/views/PeopleSearch.js
+++ public/js/views/PeopleSearch.js
@@ -9,7 +9,31 @@ alumnize.PeopleSearch = Backbone.View.extend({
     	var data = _.clone(this.model.attributes);
     	data.id = this.model.id;
     	console.log("peoplesearch data ",data);
-    	this.$el.html(this.template(data));        
+
+        $(this.el).html('<ul class="thumbnails"></ul>');
+
+        for (var i = startPos; i < endPos; i++) {
+            $('.thumbnails', this.el).append(new PeopleListItemView({model: data[i]}).render().el);
+        }
+
+        $(this.el).append(new Paginator({model: this.model, page: this.options.page}).render().el);
+
         return this;
     }
+});
+
+alumnize.PeopleListItemView = Backbone.View.extend({
+
+    tagName: "li",
+
+    initialize: function () {
+        this.model.bind("change", this.render, this);
+        this.model.bind("destroy", this.close, this);
+    },
+
+    render: function () {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
+
 });
