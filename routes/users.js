e3c4e2182c21c3e723ac31d4d2b758f81fcdd88b
niranjan <niranjan.bala@gmail.com>
2013-10-15 00:06:37 +0530

    user find

--- routes/users.js
+++ routes/users.js
@@ -117,7 +117,7 @@ exports.findByFilterAndSort = function(req, res) {
         collection.find(filters).sort(sorter).skip(pageSize * (pageNumber-1)).limit(pageSize).toArray(function(err, items) {
             res.jsonp({
                 "pageSize" : pageSize,
-                "totalCount": 10,
+                "totalCount": items.count(),
                 "pageNumber": pageNumber,
                 "result" : items
             });
