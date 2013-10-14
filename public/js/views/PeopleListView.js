9386355c24152b51c88a67eb46eb28b352055142
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-15 01:52:21 +0530

    ps

--- public/js/views/PeopleListView.js
+++ public/js/views/PeopleListView.js
@@ -25,6 +25,9 @@ alumnize.PeopleListView = Backbone.View.extend({
         _.each(data.result, function (people) {
             console.log("people : ",people);
             this.$el.append(this.template(people));
+            if(data.pageNumber == "1") {
+                document.getElementById('previous').style.display = 'none';
+            }
         }, this);
         return this;
     }
