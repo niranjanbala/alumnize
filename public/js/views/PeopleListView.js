bb28ed5071fed40f00bfb2857c8082e4eca0f1b1
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-15 16:04:52 +0530

    ps

--- public/js/views/PeopleListView.js
+++ public/js/views/PeopleListView.js
@@ -6,16 +6,7 @@ alumnize.Peoples = Backbone.Model.extend({
 
 alumnize.PeopleListView = Backbone.View.extend({
 
-    tagName:'ul',
-
-    className:'nav nav-list',
-
     initialize:function () {
-        var self = this;
-        this.model.on("reset", this.render, this);
-        this.model.on("add", function (people) {
-//            self.$el.append(new alumnize.PeopleListItemView({model:people}).render().el);
-        });
     },
 
     render:function () {
@@ -23,32 +14,6 @@ alumnize.PeopleListView = Backbone.View.extend({
         var data = _.clone(this.model.attributes);
         console.log("PeopleListView data : ",data);
         this.$el.append(this.template(data));
-       /* _.each(data.result, function (people) {
-            console.log("people : ",people);
-            this.$el.append(this.template(people));
-            if(data.pageNumber == 1) {
-               $('.previous').hide();
-            }
-        }, this); */
         return this;
     }
-});
-
-alumnize.PeopleListItemView = Backbone.View.extend({
-
-    tagName:"li",
-
-    initialize:function () {
-        this.model.on("change", this.render, this);
-        this.model.on("destroy", this.close, this);
-    },
-
-    render:function () {
-        var data = _.clone(this.model.attributes);
-        data.id = this.model.id;
-        console.log("New data :: ", data);
-        this.$el.html(this.template(data));
-        return this;
-    }
-
 });
