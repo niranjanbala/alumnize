cb4e6d9c4cb16b47c7306ad858a04b5b35366158
Niranjan Bala <niranjan.bala@coextrix.com>
2013-10-17 11:39:12 +0530

    ps

--- public/js/views/PeopleListView.js
+++ public/js/views/PeopleListView.js
@@ -12,8 +12,12 @@ alumnize.PeopleListView = Backbone.View.extend({
 		this.$el.empty();
 		var data = _.clone(this.model.attributes);
 		this.$el.html(this.template(data));
+		console.log($('#peopleList',this.$el));
+		console.log($('#peopleList',this.el));
 		_.each(this.model.get('result').models, function(user) {	
-			$('#peopleList',this.$el).html(new alumnize.UserSmallView({model:user}).render().$el);
+			var userSmallEl=new alumnize.UserSmallView({model:user}).render().el;
+			console.log(userSmallEl);
+			$('#peopleList',this.$el).html(userSmallEl);
 		});
 		console.log($('#peopleList',this.$el).html());				
         return this;
