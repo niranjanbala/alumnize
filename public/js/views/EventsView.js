bd39d15d745b81874df3b24f45534f41c03de33e
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-20 21:32:53 +0530

    events

--- public/js/views/EventsView.js
+++ public/js/views/EventsView.js
@@ -11,7 +11,7 @@ alumnize.EventsView = Backbone.View.extend({
         this.model.on("reset", this.render, this);
         this.model.on("change:result", this.render, this);
         this.model.on("add", function (user) {
-            
+            $('#dp3').datepicker('show');
             $('eventList', this.el).append(new alumnize.EventListView({model:user}).render().el);
         });
     },
