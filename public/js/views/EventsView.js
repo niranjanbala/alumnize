f0a74974f001f361ff168ddbe43a2e03142439ab
Nagaraj <ns.nagu.ngl@gmail.com>
2013-10-22 01:21:14 +0530

    events-tokeninput

--- public/js/views/EventsView.js
+++ public/js/views/EventsView.js
@@ -28,19 +28,13 @@ alumnize.EventsView = Backbone.View.extend({
         $('#endDatePicker',this.$el).datepicker({
         });
 
-        $("#demo-input-local").tokenInput([
-                {id: 7, name: "Ruby"},
-                {id: 11, name: "Python"},
-                {id: 13, name: "JavaScript"},
-                {id: 17, name: "ActionScript"},
-                {id: 19, name: "Scheme"},
-                {id: 23, name: "Lisp"},
-                {id: 29, name: "C#"},
-                {id: 31, name: "Fortran"},
-                {id: 37, name: "Visual Basic"},
-                {id: 41, name: "C"},
-                {id: 43, name: "C++"},
-                {id: 47, name: "Java"}
+        $("#location",this.$el).tokenInput([
+                {id: 1, name: "India"},
+                {id: 2, name: "China"},
+                {id: 3, name: "Australia"},
+                {id: 4, name: "Pakistan"},
+                {id: 5, name: "Japan"},
+                {id: 6, name: "Southafrica"}
             ]);
 
         return this;
